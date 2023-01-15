bd66e50bffd483f7547d175dc82bd17b65adf904
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2023-01-15 16:02:41 +0400

    remove those

--- main.py
+++ main.py
@@ -2,14 +2,11 @@ import openai
 import os
 import telethon
 from telethon.sync import TelegramClient
-from telethon.tl.types import InputPeerUser
 
 api_id = os.environ.get("API_ID")
 api_hash = os.environ.get("API_HASH")
 bot_token = os.environ.get("BOT_TOKEN")
 openai_api_key = os.environ.get("OPENAI_API_KEY")
-GROUP_ID = os.environ.get("GROUP_ID")
-OWNER_ID = os.environ.get("OWNER_ID")
 
 client = TelegramClient(session='session_name', api_id=api_id, api_hash=api_hash)
 client.start(bot_token=bot_token)
@@ -19,12 +16,12 @@ openai.api_key = openai_api_key
 # /start command
 @client.on(telethon.events.NewMessage(pattern='/start'))
 async def start_handler(event):
-    await event.respond("Hello! I am a Telegram bot powered by the OpenAI ChatGPT model.\nCreated by @HYBRID_Bots.\nTo use me Join @HYBRID_Chat and send your questions along with /ask command")
+    await event.respond("Hello! I am a Telegram bot powered by the OpenAI ChatGPT model.\nTo use me send your questions along with /ask command")
 
 # Handle all other messages
 @client.on(telethon.events.NewMessage)
 async def message_handler(event):
-    if event.message.to_id.chat_id == GROUP_ID and event.message.message.strip().startswith("/ask"):
+    if event.message.message.strip().startswith("/ask"):
         # Get the message text
         message_text = event.message.message.strip().replace("/ask","",1).strip()
 
